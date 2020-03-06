4a66bd6dd803955f91fb56ea55a24dc2f5e62cb8
Privat Bablé <bable.privat@gmail.com>
2020-03-06 04:57:15 +0100

    Exercice  01 en ligne

---
+++ Sample02/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sample02
+{
+    class Program
+    {
+        
+
+        public static void cestFini()
+        {
+            List<int> prix = new List<int>();
+            Console.WriteLine("Bobjour !");
+            String rep = "o";
+            while(rep != "n")
+            {
+                Console.WriteLine("Entrez seulement des prix sans fcfa");
+                String saisie = Console.ReadLine();
+                int conversion = int.Parse(saisie);
+                prix.Add(conversion);
+                Console.WriteLine("Voulez vous entrez un nouveau prix?");
+                rep = Console.ReadLine();
+            }
+
+            //somme
+            int somme = 0;
+            for (int i = 0; i < prix.Count; i++)
+            {
+                somme += prix[i];
+            }
+
+            //affichage
+            Console.WriteLine(somme);
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            cestFini();
+            Console.ReadKey();
+
+        }
+    }
+}
