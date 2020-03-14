000df9e917abb8183ba95ca36ed54301716bf5cd
Privat Bablé <bable.privat@gmail.com>
2020-03-15 00:12:51 +0100

    Exercice 02 en ligne

---
+++ Sample03/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Sample03
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            const int NombreMagique = 5;
+            
+            Console.WriteLine("Bonjour !");
+            
+
+            try
+            {
+                Console.WriteLine("Entrer un entier.");
+                String NbreUser = Console.ReadLine();
+                int nbreUser = int.Parse(NbreUser);
+
+                if (nbreUser > NombreMagique)
+                {
+                    Console.WriteLine("Ce nombre est trop grand");
+                }
+                else if (nbreUser < NombreMagique)
+                {
+                    Console.WriteLine("Ce nombre est trop petit");
+                }
+                else
+                {
+                    Console.WriteLine("Bravo vous avez trouver le nombre magique !");
+                }
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Vous devez entrer des nombres entiers Svp");
+            }
+        }
+    }
+}
